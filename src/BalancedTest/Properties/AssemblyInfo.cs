2dbdcf9d103b4d9364b8c2b7896a5e5ef6ba2672
William Forney <wforney@hotmail.com>
2013-09-22 16:12:47 -0700

    Updated project file name.

--- src/BalancedTest/Properties/AssemblyInfo.cs
+++ src/BalancedTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BalancedTest")]
+[assembly: AssemblyTitle("BalancedTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BalancedTest")]
+[assembly: AssemblyProduct("BalancedTests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
